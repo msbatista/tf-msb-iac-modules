2cfc61914f485e892781d848f3da50db9f19f3a0
bsmarcelo <silvabatistamarcelo@gmail.com>
2021-10-12 17:05:59 -0300

    Confgiure public ip

---
+++ modules/public_ip_address/resource/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = var.allocation_method
+
+  sku      = var.sku
+  sku_tier = var.sku_tier
+
+  availability_zone = var.allocation_method
+
+  tags = var.tags
+}
---
+++ modules/public_ip_address/resource/variables.tf
@@ -0,0 +1,43 @@
+variable "name" {
+  type        = string
+  description = "The name for this public ip."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group where this public ip will be placed."
+}
+
+variable "location" {
+  type        = string
+  description = "The location where the resource will be placed."
+}
+
+variable "allocation_method" {
+  type        = string
+  description = "Defines the allocation method for this IP address. Possible values are Static or Dynamic. Public IP Standard SKUs require allocation_method to be set to Static."
+}
+
+variable "sku" {
+  type        = string
+  default     = "Basic"
+  description = "The SKU of the Public IP. Accepted values are Basic and Standard. Defaults to Basic."
+}
+
+variable "sku_tier" {
+  type        = string
+  default     = "Regional"
+  description = "The SKU Tier that should be used for the Public IP. Possible values are Regional and Global. Defaults to Regional."
+}
+
+variable "availability_zone" {
+  type        = string
+  default     = "Zone-Redundant"
+  description = "The availability zone to allocate the Public IP in. Possible values are Zone-Redundant, 1, 2, 3, and No-Zone. Defaults to Zone-Redundant."
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = {}
+  description = "A mapping of tags which should be assigned to this resource."
+}
