f00580001d233948b55aa0fd775ae116fb376839
bsmarcelo <silvabatistamarcelo@gmail.com>
2021-10-12 14:19:08 -0300

    Add id and name to nic and rg

---
+++ modules/network_interface/resource/output.tf
@@ -0,0 +1,7 @@
+output "id" {
+  value = data.azurerm_network_interface.network_interface.id
+}
+
+output "name" {
+  value = data.azurerm_network_interface.network_interface.name
+}
--- modules/resource_group/data/output.tf
+++ modules/resource_group/data/output.tf
@@ -2,6 +2,10 @@ output "id" {
   value = data.azurerm_resource_group.resource_group.id
 }
 
+output "name" {
+  value = data.azurerm_resource_group.resource_group.name
+}
+
 output "location" {
   value = data.azurerm_resource_group.resource_group.location
 }
